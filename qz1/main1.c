9e6aa0684c0e6d4d33783348abd73cba152a68bd
b1129038 <119914340+b1129038@users.noreply.github.com>
2025-02-27 14:11:19 +0800

    Update main1.c

--- qz1/main1.c
+++ qz1/main1.c
@@ -1,3 +1,9 @@
 #include <stdio.h>
 
-int
+int main() {
+    int i = 3101;
+    int additional = i -1500;
+    if ( additional <= 0) {
+        printf("70元\n");
+    } else if (additional <= 100) {
+        printf("80元\n");
